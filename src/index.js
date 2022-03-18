55eb27b1a50f9dc061ec57a5d7c81659318429df
Nikita Starostin <starostin.450503@gmail.com>
2022-03-18 04:33:42 +0300

    set up environment variables to hide them from source code

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 ﻿import _ from 'lodash';
 import printMe from './print.js';
+import html from './test.html';
+import styles from './test.scss';
 
 function component() {
     const element = document.createElement('div');
+    const element2 = document.createElement('div');
     const btn = document.createElement('button');
+    console.log(html);
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     btn.innerHTML = 'Click me and check the console!';
+    element2.innerHTML = html;
     btn.onclick = printMe;
 
     element.appendChild(btn);
+    element.appendChild(element2)
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,46 @@
 ﻿const path = require('path');
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const DefinePlugin = require('webpack/lib/DefinePlugin');
+
+require('dotenv').config();
 
 module.exports = {
+    mode: 'development',
     entry: {
         index: './src/index.js',
         print: './src/print.js'
     },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist'
+    },
     plugins: [
+        new DefinePlugin({
+            'process.env': {
+                NEWS_API_TOKEN: JSON.stringify(process.env.NEWS_API_TOKEN)
+            }
+        }),
         new HtmlWebpackPlugin({
             title: 'Lab 4',
-        }),
+        })
     ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+            {
+                test: /\.html$/i,
+                loader: "html-loader",
+            },
+        ],
+
+    },
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
