c4a89d5943b822d4bea95ec7f1f7d5583dbab00f
Nikita Starostin <starostin.450503@gmail.com>
2022-03-18 03:23:50 +0300

    add html webpack plugin for autogeneration of index.html

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,16 @@
 ﻿const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
     entry: {
         index: './src/index.js',
         print: './src/print.js'
     },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'Lab 4',
+        }),
+    ],
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
