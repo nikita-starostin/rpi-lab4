c12679e0e7ca515b80e3eb1ff0823a51c6ebef41
Nikita Starostin <starostin.450503@gmail.com>
2022-03-18 03:21:35 +0300

    generate separate bundles and clean up the output folder before build

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 ﻿const path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: {
+        index: './src/index.js',
+        print: './src/print.js'
+    },
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true
     },
 };
